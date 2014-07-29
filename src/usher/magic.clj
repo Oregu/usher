d96b0b5f94dfdd0619af2a1b4ff2fb9b283e7bff
Oregu <oleg.burykin@gmail.com>
2014-07-29 18:24:57 +0400

    Synthesizing 'Duplicate elements' program

--- src/usher/magic.clj
+++ src/usher/magic.clj
@@ -42,13 +42,31 @@
   (usher/run
    [0 1 2 3 4 5]
    [0 1 1 2 3 5]
-   [#_{:fn =     :ar 2 :name "="  }
-    #_{:fn zero  :ar 1 :name "0"  }
-    #_{:fn one   :ar 1 :name "1"  }
-    {:fn dec   :ar 1 :name "dec"}
+   [{:fn dec   :ar 1 :name "dec"}
     {:fn #(<= % 1) :ar 1 :name "<= 1" }
     {:fn :self :ar 1 :name "fib"}
-    {:fn +     :ar 2 :name "+"  }]))
+    {:fn +     :ar 2 :name "+"  }
+
+    #_{:fn =     :ar 2 :name "="  }
+    #_{:fn zero  :ar 1 :name "0"  }
+    #_{:fn one   :ar 1 :name "1"  }]))
 
 (defn magic-fib []
   (print-p (gen-fib)))
+
+(defn gen-dupl []
+  "Generate program duplicating each element of the input list."
+  (usher/run
+   ['() '(2)   '(1 2)    ]
+   ['() '(2 2) '(1 1 2 2)]
+   [{:fn empty? :ar 1 :name "empty?"}
+    {:fn first  :ar 1 :name "car"   }
+    {:fn rest   :ar 1 :name "cdr"   }
+    {:fn :self  :ar 1 :name "dupl"  }
+    {:fn cons   :ar 2 :name "cons"  }]))
+
+(defn magic-dupl []
+  (print-p (gen-dupl)))
+
+;; Can we generate this instead?
+;; (mapcat (fn [e] (list e e)) '(1 2))
